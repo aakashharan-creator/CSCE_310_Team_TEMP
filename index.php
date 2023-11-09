3d9864f59fd5f91226c6460ad7126dba106da6ee
Svettlira <78282024+Svettlira@users.noreply.github.com>
2023-11-08 20:16:03 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Page</title>
+</head>
+<body>
+    <h2>Login</h2>
+    <form method="post" action="login_auth.php">
+        <input type="text" name="Username" placeholder="Username"><br><br>
+        <input type="password" name="Password" placeholder="Password"><br><br>
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
---
+++ login_auth.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Validate user credentials using a database (e.g., MySQL)
+
+    // Create a connection to the database
+    $conn = new mysqli('sql9.freemysqlhosting.net', 'sql9658278', 'ZX2Ybn3eNA', 'sql9658278');
+
+    // Check the connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Get input from the login form
+    $input_username = $_POST['Username'];
+    $input_password = $_POST['Password'];
+
+    // Query the database for user authentication
+    $query = "SELECT * FROM User WHERE Username = '$input_username' AND Passwords = '$input_password'";
+    $result = $conn->query($query);
+
+    // Check if a user with the given credentials exists
+    if ($result->num_rows == 1) {
+        $_SESSION['Username'] = $input_username; // Set session variable
+        // Check if the session variable is set after authentication
+        if (isset($_SESSION['Username'])) {
+            echo "Authentication successful! Welcome, " . $_SESSION['Username'];
+            // You can perform further actions or redirection here after successful authentication
+            //header("Location: index.php"); // Redirect to dashboard upon successful login
+        } else {
+            echo "Authentication failed. Invalid username or password.";
+        }
+        
+        exit();
+    } else {
+        $error = "Invalid username or password";
+    }
+
+    $conn->close(); // Close the database connection
+
+}
+?>
+<!-- ... Remaining HTML and login form -->
