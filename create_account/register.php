429f9d83b4a05233280f193631bd97dcfd8cfbf2
Svettlira <78282024+Svettlira@users.noreply.github.com>
2023-11-21 20:37:51 -0600

    Add files via upload

---
+++ create_account/register.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Create Account</title>
+</head>
+<body>
+  <h1>TEST</h1>
+  <form method="post" action="index.php">
+    <input type="text" name="Username" placeholder="Username"><br><br>
+    <input type="password" name="Password" placeholder="Password"><br><br>
+    <input type="submit" value="Submit">
+  </form>
+
+</body>
+</html>
