500c653c8a67020ca72e1dc998daff7a1e9ce43b
Svettlira <78282024+Svettlira@users.noreply.github.com>
2023-11-21 20:53:39 -0600

    Update home.php
    added redirect to 4 pages

--- user_func/home.php
+++ user_func/home.php
@@ -4,11 +4,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Student homepage</title>
 </head>
 
 <body>
     <h1>User Home Page</h1>
+    <h3>Feature 1</h3>
+        <a href="feature1.php">
+            <button>Feature 1</button> 
+        </a>
+    <h3>Feature 2</h3>
+        <a href="feature2.php">
+            <button>Feature 2</button> 
+        </a>
+    <h3>Feature 3</h3>
+        <a href="feature3.php">
+            <button>Feature 3</button> 
+        </a>
+    <h3>Feature 4</h3>
+        <a href="feature4.php">
+            <button>Feature 4</button> 
+        </a>
     
 </body>
 
