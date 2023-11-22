8232d3d51497d668bdc16bd260792df95fce3799
Svettlira <78282024+Svettlira@users.noreply.github.com>
2023-11-21 22:52:16 -0600

    Update register.php
    added all the fields needed to create a new account and fixed spacing so each text box is on a new line

--- create_account/register.php
+++ create_account/register.php
@@ -1,14 +1,62 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Create Account</title>
+  <title>Create Account</title> 
 </head>
+
 <body>
-  <h1>TEST</h1>
-  <form method="post" action="index.php">
-    <input type="text" name="Username" placeholder="Username"><br><br>
-    <input type="password" name="Password" placeholder="Password"><br><br>
-    <input type="submit" value="Submit">
+
+  <h1>Register</h1>
+
+  <form method="post" action="register_handler.php">
+
+    <div>
+      <label>UIN</label><br>
+      <input type="text" name="UIN" required>
+    </div>
+    
+    <div>
+      <label>First Name:</label><br>
+      <input type="text" name="First_name" required>
+    </div>
+
+    <div>
+      <label>Middle Initial:</label><br>
+      <input type="text" name="M_initial"> 
+    </div>
+
+    <div>
+      <label>Last Name:</label><br>   
+      <input type="text" name="Last_Name" required>
+    </div>
+
+    <div>
+      <label>Username:</label><br>
+      <input type="text" name="Username" required>
+    </div>
+
+    <div>
+      <label>Password:</label><br>
+      <input type="text" name="Password" required>
+    </div>
+    
+    <div>
+      <label>User type</label><br>
+      <input type="text" name="User_Type" required>
+    </div>
+
+    <div>
+      <label>Email:</label><br>
+      <input type="text" name="Email" required>
+    </div>
+
+    <div>
+      <label>Discord name</label><br>
+      <input type="text" name="Discord_Name" required>
+    </div>
+
+    <input type="submit" value="Register">
+
   </form>
 
 </body>
