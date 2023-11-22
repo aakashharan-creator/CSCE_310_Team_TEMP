1b8e2f587e604764ff0b4d395ee67275113674ff
Aakash Haran <aakash.haran01@gmail.com>
2023-11-22 00:24:23 -0600

    started user remove and edit feat

--- user_func/home.php
+++ user_func/home.php
@@ -9,7 +9,8 @@
 
 <body>
     <h1>User Home Page</h1>
-    
+    <a href="../index.php" style="position: absolute; top:25px; right: 25px;">Logout</a>
+    <a href="./user_profile_page/view_profile_info.php">View Information</a>
 </body>
 
 </html>
