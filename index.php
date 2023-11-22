5f2c035955ec11fad897704ec3c8f0476ebcb941
Svettlira <78282024+Svettlira@users.noreply.github.com>
2023-11-21 20:38:37 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Login Page</title>
+  <title>Home Page</title>
 </head>
 <body>
-    <h2>Login</h2>
-    <form method="post" action="user_auth/login_auth.php">
-        <input type="text" name="Username" placeholder="Username"><br><br>
-        <input type="password" name="Password" placeholder="Password"><br><br>
-        <input type="submit" value="Login">
-    </form>
+  <h1>Tamu Cybersecurity Center</h1>
+  <h2>Login</h2>
+  
+  <form method="post" action="user_auth/login_auth.php">
+    <input type="text" name="Username" placeholder="Username"><br><br>
+    <input type="password" name="Password" placeholder="Password"><br><br>
+    <input type="submit" value="Login">
+  </form>
+
+  <h3>Don't have an account?</h3>
+  <a href="create_account/register.php">
+    <button>Register</button> 
+  </a>
+
 </body>
 </html>
