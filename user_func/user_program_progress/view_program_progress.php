9a997daa0cfb6d505ad81cc85c29f5e78d455d75
Svettlira <78282024+Svettlira@users.noreply.github.com>
2023-12-01 11:29:01 -0600

    More program progress changes
    small changes in program progress front end

--- user_func/user_program_progress/view_program_progress.php
+++ user_func/user_program_progress/view_program_progress.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="view_progress.css">
+    <link rel="stylesheet" href="view_program_progress.css">
     <script src="view_program_progress.js"></script>
     <title>Document</title>
 </head>
+
 <body onload="populateUserProgramProgressData()">
     <h1>Program Progress</h1>
     <a href="../home.php" style="position: absolute; top: 25px; right: 25px;">Go back</a>
