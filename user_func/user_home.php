b7ca7e4dac24ee3a24e1b2de127fde510f9d6e4f
Svettlira <78282024+Svettlira@users.noreply.github.com>
2023-12-01 08:20:38 -0600

    Create user_home.php

---
+++ user_func/user_home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student homepage</title>
+</head>
+
+<body>
+    <a href="../index.php" style="position: absolute; top:25px; right: 25px;">Logout</a>
+    <h1>User Home Page</h1>
+    <h3>View Information</h3>
+        <a href="./user_profile_page/view_profile_info.php">
+            <button>View Information</button> 
+        </a>
+    <h3>Feature 2</h3>
+        <a href="feature2.php">
+            <button>Feature 2</button> 
+        </a>
+    <h3>Feature 3</h3>
+        <a href="feature3.php">
+            <button>Feature 3</button> 
+        </a>
+    <h3>Feature 4</h3>
+        <a href="feature4.php">
+            <button>Feature 4</button> 
+        </a>
+</body>
+</html>
