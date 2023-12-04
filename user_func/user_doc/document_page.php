de09030652b5481d11f137e0b8525039d36bc9f9
Stormblessed176 <noah.c.thompson@gmail.com>
2023-12-04 16:18:55 -0600

    document view test

---
+++ user_func/user_doc/document_page.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document Page</title>
+</head>
+
+<body>
+    <a href="../index.php" style="position: absolute; top:25px; right: 25px;">Logout</a>
+    <h1>Document Page</h1>
+
+
+    <h3>View All Documents</h3>
+        <a href="./view_docs.php">
+            <button>View Documents</button> 
+        </a>
+    <h3>Upload Documents</h3>
+        <a href="./add_docs.php">
+            <button>Add Documents</button> 
+        </a>
+    <h3>Edit Existing Documents</h3>
+        <a href="./edit_docs.php">
+            <button>Edit Documents</button> 
+        </a>
+    <h3>Delete Existing Documents</h3>
+        <a href="./delete_docs.php">
+            <button>Delete Documents</button> 
+        </a>
+</body>
+
+
+</html>
---
+++ user_func/user_doc/view_docs.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>View All Documents</title> 
+</head>
+    <body>
+    <h1>Documents</h1>
+        <a href="../home.php" style="position: absolute; top: 25px; right: 25px;">Go back</a>
+
+        <table id="user-data">
+            <tr>
+                <th>Field</th>
+                <th>Value</th>
+            </tr>
+        </table>
+    </body>
+</html>
