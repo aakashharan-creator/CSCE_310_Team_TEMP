99f8d91f17a4d7475fe67eb38cdcabfa221ff84f
Svettlira <78282024+Svettlira@users.noreply.github.com>
2023-12-03 20:44:09 -0600

    Delete cert added
    Delete cert added

---
+++ user_func/user_program_progress/delete_pp/delete_cert_page.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Delete existing certification</title> 
+</head>
+    <body>
+    <h1> Delete existing certification </h1>
+      <p> Make sure to put the right CertE_Num to edit the correct record </p>
+      <form method="post" action="delete_cert_data.php">
+
+      <div>
+          <div>
+          <label>CertE_Num</label><br>
+          <input type="text" name="CertE_Num" required>
+      </div>
+
+      <input type="submit" value="submit">
+
+      </form>
+    </body>
+</html>
