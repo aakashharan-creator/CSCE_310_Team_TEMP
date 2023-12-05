b2e166930d4527a639c97884188a15579f9dbd5a
Svettlira <78282024+Svettlira@users.noreply.github.com>
2023-12-05 13:59:23 -0600

    admin edit internship added

---
+++ admin_func/student_program_progress/edit_pp/edit_internship_page.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Edit a students internship record</title> 
+</head>
+    <body>
+    <h1> Edit existing Internship </h1>
+      <p> Make sure to put the right IA_Num to edit the correct record </p>
+      <form method="post" action="edit_internship_data.php">
+
+      <div>
+          <label>IA_Num</label><br>
+          <input type="text" name="IA_Num" required>
+          </div>
+
+          <label>UIN</label><br>
+          <input type="text" name="UIN" required>
+          </div>
+
+          <div>
+          <label>Intern_ID</label><br>
+          <input type="text" name="Intern_ID" required>
+          </div>
+
+          <div>
+          <label>Status</label><br>
+          <input type="text" name="Status" required>
+          </div>
+
+          <div>
+          <label>Year</label><br>
+          <input type="text" name="Year" required> 
+          </div>
+          
+          <input type="submit" value="submit">
+
+      </form>
+    </body>
+</html>
