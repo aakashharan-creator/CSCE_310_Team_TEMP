1c8b02faf7dd2be1d294b670343c538670027c2a
Aakash Haran <aakash.haran01@gmail.com>
2023-12-06 17:57:41 -0600

    finished fixing view,index user page now shows all data

--- user_func/user_profile_page/get_user_data.php
+++ user_func/user_profile_page/get_user_data.php
@@ -7,11 +7,15 @@ if ($conn->connect_error) {
     echo "$conn->connect_error";
     die("Connectaion Failed : " . $conn->connect_error);
 } else {
-    $query = "SELECT * FROM User use index(uin_idx) JOIN College_Student CS ON User.UIN = CS.UIN with  WHERE User.Username = '" . $_SESSION['Username'] . "';";
+    $query = "SELECT * FROM User use index(uin_idx) JOIN College_Student CS ON User.UIN = CS.UIN  WHERE User.Username = '" . $_SESSION['Username'] . "';";
     $result = mysqli_query($conn, $query);
     
     $row = $result->fetch_assoc();
-    $user_data = array($row["Username"], $row["First_Name"], $row["M_Initial"], $row["Last_Name"], $row["Email"], $row["Discord_Name"]);
+    $user_data = array($row["Username"], $row["First_Name"], $row["M_Initial"], $row["Last_Name"], 
+    $row["Email"], $row["Discord_Name"], $row["UIN"], $row["Gender"], $row["Hispanic_or_Latino"], 
+    $row["Race"], $row["US_Citizen"], $row["First_Generation"], $row["GPA"], $row["Major"], $row["Minor_1"], 
+    $row["Minor_2"], $row["Expected_Graduation"], $row["School"], $row["Classification"], $row["Phone"], 
+    $row["Student_Type"]);
 }
 
 echo json_encode($user_data);
--- user_func/user_profile_page/user_home_data.js
+++ user_func/user_profile_page/user_home_data.js
@@ -4,7 +4,7 @@ function populateUserData() {
     fetch("get_user_data.php").then((response) => {
         return response.json();
     }).then((data) => {
-        let keys = ["Username", "First Name", "M Initial", "Last Name", "Email", "Discord"];
+        let keys = ["Username", "First Name", "M Initial", "Last Name", "Email", "Discord", "UIN", "Gender", "Hispanic_or_Latino", "Race", "US_Citizen", "First_Generation", "GPA", "Major", "Minor_1", "Minor_2", "Expected_Graduation", "School", "Classification", "Phone", "Student_Type"];
 
         for (let i = 0; i < keys.length; i++) {
             let row = document.createElement("tr");
